a232c79fac751a67a9265a208b9541e8c82826a9
theEdgeBreaker <thesanjana2002@gmail.com>
2024-04-07 18:13:05 +0530

    add useUser.tsx

---
+++ hooks/useUser.tsx
@@ -0,0 +1,25 @@
+import { User } from "@supabase/auth-helpers-nextjs"
+import { useSessionContext } from "@supabase/auth-helpers-react"
+import { createContext } from "react"
+
+import {Subscription, UserDetails} from '@/types'
+
+type UserContextType = {
+    accessToken: string | null;
+    user: User | null;
+    userDetails: UserDetails | null;
+    isLoading: boolean;
+    subscription: Subscription | null;
+}
+
+export const UserContext = createContext<UserContextType | undefined>(
+    undefined
+);
+
+export interface Props {
+    [propName: string]: any;    
+}
+
+export const MyUserContextProvider = (props: props) => {
+    const {} = useSessionContext()
+}
